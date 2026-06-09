6eebd9c766f1b86534c216a5b9e38e9cd00d7791
F1Justin <f1justin@outlook.com>
2026-06-09 18:21:25 +0800

    feat: add friends section on stable homepage
    Revert the unpublished site expansion back to the stable homepage baseline, add the Chesszyh friend link, refresh social icons, and update dependencies.

---
+++ src/data/friends.ts
@@ -0,0 +1,19 @@
+type Friend = {
+  name: string;
+  url: string;
+  description: string;
+  hoverNote: string;
+  avatar?: string;
+  initial: string;
+};
+
+export const friends: Friend[] = [
+  {
+    name: "Chesszyh的博客",
+    url: "https://blog.chesszyh.xyz",
+    description: "\"Welcome to Chesszyh's Blog!\"",
+    hoverNote: "我的好友，是国际象棋大神喵",
+    avatar: "https://avatars.githubusercontent.com/u/147014861",
+    initial: "C"
+  }
+];
