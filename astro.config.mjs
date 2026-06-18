210ddedcac76dfe154480a9113db8bf193899a3f
F1Justin <f1justin@outlook.com>
2026-06-18 14:22:29 +0800

    feat: improve brand SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://f1justin.com',
+  site: 'https://www.f1justin.com',
   compressHTML: true,
   integrations: [react()],
 });
